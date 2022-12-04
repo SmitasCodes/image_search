b9e23f1b92c687d35b91a4b91b059cae504323f3
SmitasCodes <airidassmith30@gmail.com>
2022-12-05 01:02:02 +0200

    funkciunuolumas kaip ir padarytas, veikia gerai, reikia sutvarkyti grid.

---
+++ script.js
@@ -0,0 +1,35 @@
+const main = document.querySelector("#main");
+function fetchImages() {
+    main.innerHTML = "";
+    event.preventDefault();
+    let input = document.querySelector("#input").value;
+    let url = "https://api.unsplash.com/search/photos?page=1&per_page=40&query=";
+    const clientID = "&client_id=jmzALUdqPjI35eGjTFB6eKSIFrKD1tP_ZJgRnq3w59M";
+
+    if (input.length <= 0) {
+        alert("Please enter what kind of images you searching for!")
+    } else {
+        let img = document.createElement('img');
+        fetch(url + input + clientID, {
+            method: 'GET'
+        })
+            .then((response) => response.json())
+            .then((images) => {
+                for (let i = 0; i < images.results.length; i++) {
+                    let img = document.createElement("img");
+                    img.src = images.results[i].urls.small;
+                    main.appendChild(img)
+                }
+            })
+    }
+}
+
+// Potentialy useful code
+
+// let div = document.createElement("div");
+// div.classList.add("column")
+// main.appendChild(div)
+// let img = document.createElement("img");
+ // img.src = images.results[i].urls.small;
+// main.appendChild(img)
+                    
